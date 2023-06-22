3321160796a272639ccf5f891cff1d1435d13306
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2023-06-22 16:08:32 +0000

    Fixed paths in micro bench cluster script

--- training-dataset/microbench_job.sh
+++ training-dataset/microbench_job.sh
@@ -14,14 +14,13 @@
 runs=$1
 mem_freq=$2
 core_freq=$3
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-srun --output=$SCRIPT_DIR/logs/ArithLocalMixed_${mem_freq}_${core_freq}.log $SCRIPT_DIR/sycl-bench/build/ArithLocalMixed --device=gpu --num-runs=$runs --size=65536 --num-iters=20000 --memory-freq=${mem_freq} --core-freq=${core_freq}
-srun --output=$SCRIPT_DIR/logs/ArithMixedUnitOp_${mem_freq}_${core_freq}.log $SCRIPT_DIR/sycl-bench/build/ArithMixedUnitOp --device=gpu --num-runs=$runs --size=500000 --num-iters=50000 --memory-freq=${mem_freq} --core-freq=${core_freq}
-srun --output=$SCRIPT_DIR/logs/ArithMixedUnitType_${mem_freq}_${core_freq}.log $SCRIPT_DIR/sycl-bench/build/ArithMixedUnitType --device=gpu --num-runs=$runs --size=500000 --num-iters=25000 --memory-freq=${mem_freq} --core-freq=${core_freq}
-srun --output=$SCRIPT_DIR/logs/ArithSingleUnit_${mem_freq}_${core_freq}.log $SCRIPT_DIR/sycl-bench/build/ArithSingleUnit --device=gpu --num-runs=$runs --size=500000 --num-iters=500000 --memory-freq=${mem_freq} --core-freq=${core_freq}
-srun --output=$SCRIPT_DIR/logs/GlobalMemory_${mem_freq}_${core_freq}.log $SCRIPT_DIR/sycl-bench/build/GlobalMemory --device=gpu --num-runs=$runs --size=1048576 --num-iters=1000000 --memory-freq=${mem_freq} --core-freq=${core_freq}
-srun --output=$SCRIPT_DIR/logs/L2Unit_${mem_freq}_${core_freq}.log $SCRIPT_DIR/sycl-bench/build/L2Unit --device=gpu --num-runs=$runs --size=1000000 --num-iters=131072 --memory-freq=${mem_freq} --core-freq=${core_freq}
-srun --output=$SCRIPT_DIR/logs/LocalMemory_${mem_freq}_${core_freq}.log $SCRIPT_DIR/sycl-bench/build/LocalMemory --device=gpu --num-runs=$runs --size=1000000 --num-iters=1000000 --memory-freq=${mem_freq} --core-freq=${core_freq}
-srun --output=$SCRIPT_DIR/logs/GlobalMemory2_${mem_freq}_${core_freq}.log $SCRIPT_DIR/sycl-bench/build/GlobalMemory2 --device=gpu --num-runs=$runs --size=262144 --num-iters=200 --memory-freq=${mem_freq} --core-freq=${core_freq}
-srun --output=$SCRIPT_DIR/logs/Stencil_${mem_freq}_${core_freq}.log $SCRIPT_DIR/sycl-bench/build/Stencil --device=gpu --num-runs=$runs --size=8192 --num-iters=15 --memory-freq=${mem_freq} --core-freq=${core_freq}
+srun --output=./logs/ArithLocalMixed_${mem_freq}_${core_freq}.log ./sycl-bench/build/ArithLocalMixed --device=gpu --num-runs=$runs --size=65536 --num-iters=20000 --memory-freq=${mem_freq} --core-freq=${core_freq}
+srun --output=./logs/ArithMixedUnitOp_${mem_freq}_${core_freq}.log ./sycl-bench/build/ArithMixedUnitOp --device=gpu --num-runs=$runs --size=500000 --num-iters=50000 --memory-freq=${mem_freq} --core-freq=${core_freq}
+srun --output=./logs/ArithMixedUnitType_${mem_freq}_${core_freq}.log ./sycl-bench/build/ArithMixedUnitType --device=gpu --num-runs=$runs --size=500000 --num-iters=25000 --memory-freq=${mem_freq} --core-freq=${core_freq}
+srun --output=./logs/ArithSingleUnit_${mem_freq}_${core_freq}.log ./sycl-bench/build/ArithSingleUnit --device=gpu --num-runs=$runs --size=500000 --num-iters=500000 --memory-freq=${mem_freq} --core-freq=${core_freq}
+srun --output=./logs/GlobalMemory_${mem_freq}_${core_freq}.log ./sycl-bench/build/GlobalMemory --device=gpu --num-runs=$runs --size=1048576 --num-iters=1000000 --memory-freq=${mem_freq} --core-freq=${core_freq}
+srun --output=./logs/L2Unit_${mem_freq}_${core_freq}.log ./sycl-bench/build/L2Unit --device=gpu --num-runs=$runs --size=1000000 --num-iters=131072 --memory-freq=${mem_freq} --core-freq=${core_freq}
+srun --output=./logs/LocalMemory_${mem_freq}_${core_freq}.log ./sycl-bench/build/LocalMemory --device=gpu --num-runs=$runs --size=1000000 --num-iters=1000000 --memory-freq=${mem_freq} --core-freq=${core_freq}
+srun --output=./logs/GlobalMemory2_${mem_freq}_${core_freq}.log ./sycl-bench/build/GlobalMemory2 --device=gpu --num-runs=$runs --size=262144 --num-iters=200 --memory-freq=${mem_freq} --core-freq=${core_freq}
+srun --output=./logs/Stencil_${mem_freq}_${core_freq}.log ./sycl-bench/build/Stencil --device=gpu --num-runs=$runs --size=8192 --num-iters=15 --memory-freq=${mem_freq} --core-freq=${core_freq}
